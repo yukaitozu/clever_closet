167cdf445e235e8c4c1e8f6557aed2f5ad802772
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-03-03 14:40:08 +0900

    setting up cloudinary and key

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,70 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_094141) do
+ActiveRecord::Schema.define(version: 2020_03_03_053707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "donations", force: :cascade do |t|
+    t.date "pick_up_date"
+    t.string "address"
+    t.boolean "picked_up"
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_donations_on_item_id"
+    t.index ["user_id"], name: "index_donations_on_user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "size"
+    t.string "location"
+    t.boolean "donated"
+    t.boolean "lent"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "items_looks", id: false, force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "look_id"
+    t.index ["item_id"], name: "index_items_looks_on_item_id"
+    t.index ["look_id"], name: "index_items_looks_on_look_id"
+  end
+
+  create_table "looks", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_looks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +87,9 @@ ActiveRecord::Schema.define(version: 2020_03_02_094141) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "donations", "items"
+  add_foreign_key "donations", "users"
+  add_foreign_key "items", "users"
+  add_foreign_key "looks", "users"
 end
