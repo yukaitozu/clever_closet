772f5d874ced24714748f921c110d00979e440a7
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-03-04 16:30:13 +0900

    added separated results for user items and friends items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,16 @@ class ItemsController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index
   def index
     # @items = Item.all
-    @items = Item.all.where(user: current_user)
+    
 
     if params[:query].present?
       # @items = Item.where(user: current_user.friends).or(Item.where(user: current_user)).search_all_items(params[:query])
-      @items = Item.owned_by(current_user).or(Item.owned_by(current_user.friends)).search_all_items(params[:query])
+      searched_items = Item.owned_by(current_user).or(Item.owned_by(current_user.friends)).search_all_items(params[:query])
+      @items = searched_items.where(user: current_user)
+      @friends_items = searched_items.where.not(user: current_user)
+    else
+      @items = Item.all.where(user: current_user)
+      
     end
 
   end
