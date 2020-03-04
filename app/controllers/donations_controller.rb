8a63c4ce7158287b3915ba829c18560c2f5b6012
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-03-04 12:14:19 +0900

    setting donations routes

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,10 @@
+class DonationsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:show]
-  resources :items, only: [:index, :show]
-
+  resources :items, only: [:show] do
+    resources :donations, only: [:new, :create]
+  end
+  resources :items, only: [:index]
 end
