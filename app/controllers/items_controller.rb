48b436e5a58a330668a2a5757c898ae9cf66ba71
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-03-04 11:39:11 +0900

    added scope owned_by to refine search in items index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,15 @@
 class ItemsController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:show, :index]
-
+skip_after_action :verify_policy_scoped, only: :index
   def index
-    @items = Item.all
-    @items = policy_scope(Item)
+    # @items = Item.all
+    @items = Item.all.where(user: current_user)
 
     if params[:query].present?
-      @items = Item.search_all_items(params[:query])
+      # @items = Item.where(user: current_user.friends).or(Item.where(user: current_user)).search_all_items(params[:query])
+      @items = Item.owned_by(current_user).or(Item.owned_by(current_user.friends)).search_all_items(params[:query])
     end
+
   end
 
   def show
