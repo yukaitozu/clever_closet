e83361122704dbc9635956b150420aded0e29849
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-03-05 19:36:12 +0900

    merge completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_03_05_025029) do
-=======
 ActiveRecord::Schema.define(version: 2020_03_05_092454) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
