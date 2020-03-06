85bf5d7130986f63c43a09b6fe1aa09230d413b1
Yuka Itozu <yukaitozu@gmail.com>
2020-03-06 16:26:22 +0900

    switched ac to herokuapp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'wss://clever-closet.me/cable'
+  config.action_cable.url = 'wss://clever-closet.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'http://clever-closet.me', 'https://clever-closet.me', 'https://clever-closet.herokuapp.com', 'http://clever-closet.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
