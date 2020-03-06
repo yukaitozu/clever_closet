c108d1bf176fd3f1710e79ac545d54adbde95263
Yuka Itozu <yukaitozu@gmail.com>
2020-03-06 11:18:44 +0900

    fixed navbar so that it isn't covered

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
-
 import "bootstrap";
 
 import { initUpdateNavbarOnScroll } from '../components/top-navbar';
