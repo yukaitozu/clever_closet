3ae5077ac9c17060442c9f2d7f8837fbd98444fa
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-03-06 16:58:54 +0900

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require "open-uri"
 
+ChatRoom.destroy_all
 Donation.destroy_all
 Item.destroy_all
 Look.destroy_all
 User.destroy_all
+
 
 puts "creating Users"
 # erica = User.create!(
@@ -52,7 +54,8 @@ red = Item.create!(
   name: "Fluffy sweater",
   size: "M",
   user: soda,
-  category: Item::CATEGORIES.sample
+  category: Item::CATEGORIES[0],
+  tag_list: "red"
   )
 file = URI.open('https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2018/10/15/goods-first-img/1569374114760435886.jpg')
 red.photo.attach(io: file, filename: 'red_sweater.jpg', content_type: 'image/jpg')
@@ -61,7 +64,7 @@ purple = Item.create!(
   name: "Comfy sweater",
   size: "M",
   user: noemi,
-  category: Item::CATEGORIES.sample
+  category: Item::CATEGORIES[0],
   )
 file = URI.open('https://lp2.hm.com/hmgoepprod?set=quality[79],source[/b6/04/b604dfe61163cd67499fac3895220c8620cc29cb.jpg],origin[dam],category[ladies_cardigansjumpers_jumpers],type[DESCRIPTIVESTILLLIFE],res[m],hmver[4]&call=url[file:/product/main]')
 purple.photo.attach(io: file, filename: 'purple_sweater.jpg', content_type: 'image/jpg')
@@ -70,7 +73,8 @@ red_tshirt = Item.create!(
   name: "Summer t-shirt",
   size: "M",
   user: noemi,
-  category: Item::CATEGORIES.sample
+  category: Item::CATEGORIES[0],
+  tag_list: "red"
   )
 file = URI.open('https://5.imimg.com/data5/GT/KL/ML/SELLER-61610499/women-red-plain-t-shirt-500x500.jpg')
 red_tshirt.photo.attach(io: file, filename: 'red_tshirt.jpg', content_type: 'image/jpg')
@@ -82,7 +86,7 @@ jeans = Item.create!(
   name: "Jeans",
   size: "S",
   user: noemi,
-  category: Item::CATEGORIES.sample
+  category: Item::CATEGORIES[1]
   )
 file2 = URI.open('https://www.kmart.com.au/wcsstore/Kmart/images/ncatalog/f/4/69069734-1-f.jpg')
 jeans.photo.attach(io: file2, filename: 'jeans.jpg', content_type: 'image/jpg')
@@ -91,7 +95,7 @@ skirt = Item.create!(
   name: "Long skirt",
   size: "M",
   user: noemi,
-  category: Item::CATEGORIES.sample
+  category: Item::CATEGORIES[1]
   )
 file2 = URI.open('https://images.yoox.com/35/35422428ke_12_f.jpg')
 skirt.photo.attach(io: file2, filename: 'skirt.jpg', content_type: 'image/jpg')
@@ -100,7 +104,8 @@ crop_top = Item.create!(
   name: "Crop top",
   size: "S",
   user: noemi,
-  category: Item::CATEGORIES.sample
+  category: Item::CATEGORIES[0],
+  tag_list: "red"
   )
 file2 = URI.open('https://picture-cdn.wheretoget.it/c9xapf-l-610x610-tank-bustier-crop+tops-red-red+crop-red+bustier-crop+bustier-cutout+crop-cut+crop-bralette-crop+tank-crop+tanks.jpg')
 crop_top.photo.attach(io: file2, filename: 'crop_top.jpg', content_type: 'image/jpg')
@@ -111,7 +116,7 @@ docs = Item.create!(
   name: "Doc Martens",
   size: "M",
   user: noemi,
-  category: Item::CATEGORIES.sample
+  category: Item::CATEGORIES[2]
   )
 file3 = URI.open('https://images-na.ssl-images-amazon.com/images/I/61nTdJxhgXL._AC_UY500_.jpg')
 docs.photo.attach(io: file3, filename: 'docs.jpg', content_type: 'image/jpg')
@@ -120,7 +125,7 @@ hoodie = Item.create!(
   name: "Hoodie",
   size: "L",
   user: noemi,
-  category: Item::CATEGORIES.sample
+  category: Item::CATEGORIES[0]
   )
 file3 = URI.open('https://imcut.jollychic.com//uploads/jollyimg/imageLibrary/201710/46Q/27/IL201710271442480873.jpg')
 hoodie.photo.attach(io: file3, filename: 'hoodie.jpg', content_type: 'image/jpg')
@@ -129,7 +134,7 @@ blouse = Item.create!(
   name: "Blouse",
   size: "M",
   user: noemi,
-  category: Item::CATEGORIES.sample
+  category: Item::CATEGORIES[0]
   )
 file3 = URI.open('https://img.mytheresa.com/1088/1088/66/jpeg/catalog/product/27/P00395096.jpg')
 blouse.photo.attach(io: file3, filename: 'blouse.jpg', content_type: 'image/jpg')
