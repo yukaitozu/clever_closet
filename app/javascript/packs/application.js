7efe8cd9eb8b5ff82b0acf87a0b905d0c2c25d26
Yuka Itozu <yukaitozu@gmail.com>
2020-03-07 16:48:39 +0900

    added autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,13 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initUpdateNavbarOnScroll } from '../components/top-navbar';
 import 'plugins/flatpickr';
 import {scrollLastMessageIntoView} from '../components/scroll';
+
+
+initAutocomplete();
 
 // NavBar Scroll
 initUpdateNavbarOnScroll();
