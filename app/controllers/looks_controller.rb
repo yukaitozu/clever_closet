447e5c22e3ff657c41c55e52943e75eb368d6b9b
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-03-09 17:21:37 +0900

    added links to profile pages to go to the right new look routes

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -3,8 +3,8 @@ class LooksController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index
 
   def index
-    @my_looks = Look.all.where(user: current_user)
-    @friends_looks = Look.all.where.not(user: current_user)
+    @my_looks = current_user.my_looks
+    @friends_looks = current_user.friends_looks
   end
 
   def show
@@ -12,6 +12,7 @@ class LooksController < ApplicationController
   end
 
   def new
+    @user = params[:user_id] ? User.find(params[:user_id]) : current_user
     @look = Look.new
     @items = Item.where(user: current_user)
     authorize @look
@@ -19,7 +20,7 @@ class LooksController < ApplicationController
 
   def create
     @look = Look.new(look_params)
-    @look.user = current_user
+    @look.user = User.find(params[:user_id]) || current_user
     authorize @look
     if @look.save
       redirect_to edit_look_path(@look)
@@ -30,21 +31,12 @@ class LooksController < ApplicationController
 
   def edit
     authorize @look
-   
-    @bottoms = Item.where(user: current_user, category: "bottom")
-    @shoes = Item.where(user: current_user, category: "shoes")
-    @outers = Item.where(user: current_user, category: "outerwear")
-    @accessories = Item.where(user: current_user, category: "accessory")
-
   end
 
   def update
-    
-    @bottoms = Item.where(user: current_user, category: "bottom")
-    @shoes = Item.where(user: current_user, category: "shoes")
-    @outers = Item.where(user: current_user, category: "outerwear")
-    @accessories = Item.where(user: current_user, category: "accessory")
+    # removes all previously selected items
     @look.items = []
+    # shovel new selected items into items array
     @look.items << params[:look][:item_ids].reject{ |item_id| item_id == "" }.map { |item_id| Item.find(item_id)}
     authorize @look
     if @look.save
@@ -52,7 +44,6 @@ class LooksController < ApplicationController
     else
       render :edit
     end
-
   end
 
   private
