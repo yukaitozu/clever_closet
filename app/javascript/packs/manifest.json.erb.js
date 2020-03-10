08a2f684c0514a3fb64377edca8e921fce0c63d0
Yuka Itozu <yukaitozu@gmail.com>
2020-03-10 16:57:48 +0900

    working on pwa

---
+++ app/javascript/packs/manifest.json.erb.js
@@ -0,0 +1,17 @@
+{
+  "background_color": "white",
+  "description": "Clever Closet - Smaller Closets, Bigger Impact",
+  "display": "fullscreen",
+  "icons": [
+    {
+      "src": "<%= image_path 'icons/android-icon-192x192.png' %>",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {},
+    {}
+  ],
+  "name": "Clever Closet",
+  "short_name": "Clever Closet",
+  "start_url": "/"
+}
