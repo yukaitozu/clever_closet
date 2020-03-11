c44a3bffc01970339bd256d8a5fd096bdf3f4580
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-03-11 15:20:56 +0900

    added validations to have just one chat per friendship

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -2,4 +2,13 @@ class ChatRoom < ApplicationRecord
   belongs_to :user_one, class_name: "User"
   belongs_to :user_two, class_name: "User"
   has_many :messages, dependent: :destroy
+
+  validate :check_duplicates
+  validates_uniqueness_of :user_one, scope: :user_two
+
+  def check_duplicates
+    if ChatRoom.find_by(user_one: user_two, user_two: user_one)
+      errors.add(:user_one, "Chat room already exists")
+    end
+  end
 end
