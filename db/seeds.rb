ec804a78fe8c4e4fe48f560fd13b2b22dcefd058
Yuka Itozu <yukaitozu@gmail.com>
2020-03-12 10:51:56 +0900

    added bottoms

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,6 @@ yuka = User.create!(
 file = URI.open('https://avatars1.githubusercontent.com/u/40887017?s=460&v=4')
 yuka.photo.attach(io: file, filename: 'yuka.jpg', content_type: 'image/jpg')
 
-
 yuichiro = User.create!(
   username: "Yuichiro",
   email: "yuichiro@gmail.com",
@@ -47,7 +46,6 @@ soda = User.create!(
 )
 file = URI.open('https://avatars1.githubusercontent.com/u/6465116?s=460&v=4')
 soda.photo.attach(io: file, filename: 'soda.jpg', content_type: 'image/jpg')
-
 
 erica = User.create!(
   username: "Erica",
@@ -73,6 +71,7 @@ brad = User.create!(
 file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975947/boy_qomnkt.jpg')
 brad.photo.attach(io: file, filename: 'boy.jpg', content_type: 'image/jpg')
 
+
 puts "creating shirts"
 
 striped_shirt = Item.create!(
@@ -95,207 +94,127 @@ white_tshirt = Item.create!(
 file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975704/white-shirt.png')
 white_tshirt.photo.attach(io: file, filename: 'white-shirt.jpg', content_type: 'image/jpg')
 
-purple = Item.create!(
-  name: "Comfy sweater",
+teal_sweater = Item.create!(
+  name: "Teal Sweater",
   size: "M",
   user: noemi,
   category: Item::CATEGORIES[0],
+  tag_list: "teal"
   )
-file = URI.open('https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/400452/item/goods_72_400452.jpg?width=734')
-purple.photo.attach(io: file, filename: 'purple_sweater.jpg', content_type: 'image/jpg')
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975704/teal_sweater.png')
+teal_sweater.photo.attach(io: file, filename: 'teal_sweater.jpg', content_type: 'image/jpg')
 
-red_tshirt = Item.create!(
-  name: "Summer t-shirt",
+blue_sweater = Item.create!(
+  name: "Blue Sweater",
   size: "M",
   user: noemi,
   category: Item::CATEGORIES[0],
-  tag_list: "red"
+  tag_list: "blue"
   )
-file = URI.open('https://5.imimg.com/data5/GT/KL/ML/SELLER-61610499/women-red-plain-t-shirt-500x500.jpg')
-red_tshirt.photo.attach(io: file, filename: 'red_tshirt.jpg', content_type: 'image/jpg')
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975703/blue_sweater.png')
+blue_sweater.photo.attach(io: file, filename: 'blue_sweater.jpg', content_type: 'image/jpg')
+
+white_shirt = Item.create!(
+  name: "White Shirt",
+  size: "M",
+  user: yuka,
+  category: Item::CATEGORIES[0],
+  tag_list: "white"
+  )
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975703/white_shirt.png')
+white_shirt.photo.attach(io: file, filename: 'white_shirt.jpg', content_type: 'image/jpg')
 
 
-puts "creating jeans"
+puts "creating botoms"
 
-jeans = Item.create!(
+yuichiro_jeans = Item.create!(
   name: "Jeans",
-  size: "S",
-  user: noemi,
-  category: Item::CATEGORIES[1]
-  )
-file2 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/427456/item/64_427456.jpg')
-jeans.photo.attach(io: file2, filename: 'jeans.jpg', content_type: 'image/jpg')
-
-skirt = Item.create!(
-  name: "Long skirt",
   size: "M",
-  user: noemi,
+  user: yuichiro,
   category: Item::CATEGORIES[1]
+  tag_list: "jeans"
   )
-file2 = URI.open('https://images.yoox.com/35/35422428ke_12_f.jpg')
-skirt.photo.attach(io: file2, filename: 'skirt.jpg', content_type: 'image/jpg')
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975704/yuichiro-jeans.png')
+yuichiro_jeans.photo.attach(io: file, filename: 'yuichiro-jeans.jpg', content_type: 'image/jpg')
 
-crop_top = Item.create!(
-  name: "Crop top",
-  size: "S",
-  user: noemi,
-  category: Item::CATEGORIES[0],
-  tag_list: "red"
-  )
-file2 = URI.open('https://picture-cdn.wheretoget.it/c9xapf-l-610x610-tank-bustier-crop+tops-red-red+crop-red+bustier-crop+bustier-cutout+crop-cut+crop-bralette-crop+tank-crop+tanks.jpg')
-crop_top.photo.attach(io: file2, filename: 'crop_top.jpg', content_type: 'image/jpg')
-
-
-
-docs = Item.create!(
-  name: "Doc Martens",
+pink_trousers = Item.create!(
+  name: "Pink Trousers",
   size: "M",
-  user: noemi,
+  user: soda,
+  category: Item::CATEGORIES[1]
+  tag_list: "pink"
+  )
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975704/pink_trousers.png')
+pink_trousers.photo.attach(io: file, filename: 'pink_trousers.jpg', content_type: 'image/jpg')
+
+brown_pants = Item.create!(
+  name: "Brown Pants",
+  size: "M",
+  user: soda,
+  category: Item::CATEGORIES[1]
+  tag_list: "brown"
+  )
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975703/brown_pants.png')
+brown_pants.photo.attach(io: file, filename: 'brown_pants.jpg', content_type: 'image/jpg')
+
+
+
+puts "creating shoes"
+
+star_shoes = Item.create!(
+  name: "Star Shoes",
+  size: "M",
+  user: soda,
   category: Item::CATEGORIES[2]
+  tag_list: "blue"
   )
-file3 = URI.open('https://images-na.ssl-images-amazon.com/images/I/61nTdJxhgXL._AC_UY500_.jpg')
-docs.photo.attach(io: file3, filename: 'docs.jpg', content_type: 'image/jpg')
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975704/star-shoes.png')
+star_shoes.photo.attach(io: file, filename: 'star-shoes.jpg', content_type: 'image/jpg')
 
-hoodie = Item.create!(
-  name: "Hoodie",
-  size: "L",
-  user: noemi,
-  category: Item::CATEGORIES[0]
-  )
-file3 = URI.open('https://imcut.jollychic.com//uploads/jollyimg/imageLibrary/201710/46Q/27/IL201710271442480873.jpg')
-hoodie.photo.attach(io: file3, filename: 'hoodie.jpg', content_type: 'image/jpg')
-
-blouse = Item.create!(
-  name: "Blouse",
-  size: "M",
-  user: noemi,
-  category: Item::CATEGORIES[0]
-  )
-file3 = URI.open('https://img.mytheresa.com/1088/1088/66/jpeg/catalog/product/27/P00395096.jpg')
-blouse.photo.attach(io: file3, filename: 'blouse.jpg', content_type: 'image/jpg')
-
-puts "extra seeds"
-
-red_uniqulo_sweater = Item.create!(
-  name: "red-turtleneck-sweater",
+sneakers = Item.create!(
+  name: "Running Shoes",
   size: "M",
   user: soda,
-  category: Item::CATEGORIES[0],
+  category: Item::CATEGORIES[2]
+  tag_list: "sneakers"
   )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/41822900001/item/17_41822900001.jpg')
-red_uniqulo_sweater.photo.attach(io: file4, filename: 'red_uniqulo_sweater.jpg', content_type: 'image/jpg')
-p 1
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975703/sneakers.png')
+sneakers.photo.attach(io: file, filename: 'sneakers.jpg', content_type: 'image/jpg')
 
-black_jacket = Item.create!(
-  name: "black_jacket",
+
+puts "creating outerwear"
+
+suit_jacket = Item.create!(
+  name: "Formal Jacket",
   size: "M",
   user: soda,
-  category: Item::CATEGORIES[0],
+  category: Item::CATEGORIES[3],
   tag_list: "black"
   )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/426699/sub/426699_sub7_popup.jpg')
-black_jacket.photo.attach(io: file4, filename: 'black_jacket.jpg', content_type: 'image/jpg')
-p 2
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975704/suit_jacket.png')
+suit_jacket.photo.attach(io: file, filename: 'suit_jacket.jpg', content_type: 'image/jpg')
 
-beije_t_shirts = Item.create!(
-  name: "beije-t-shirts",
+black_jacket = Item.create!(
+  name: "Black Jacket",
   size: "M",
   user: soda,
-  category: Item::CATEGORIES[0],
+  category: Item::CATEGORIES[3],
+  tag_list: "black"
   )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/424024/sub/424024_sub7_popup.jpg')
-beije_t_shirts.photo.attach(io: file4, filename: 'beije_t_shirts', content_type: 'image/jpg')
-p 3
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975704/black_jacket.png')
+black_jacket.photo.attach(io: file, filename: 'black_jacket.jpg', content_type: 'image/jpg')
 
-bag = Item.create!(
-  name: "bag_white",
-  size: "M",
-  user: soda,
-  category: Item::CATEGORIES[4],
-  )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/422009/item/00_422009.jpg')
-bag.photo.attach(io: file4, filename: 'bag_white', content_type: 'image/jpg')
-p 4
 
-belt = Item.create!(
-  name: "belt_brown",
+puts "creating accessories"
+
+glasses = Item.create!(
+  name: "Glasses",
   size: "M",
   user: soda,
   category: Item::CATEGORIES[4],
   )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/426854/item/35_426854.jpg')
-belt.photo.attach(io: file4, filename: 'belt_brown', content_type: 'image/jpg')
-p 5
-
-yellow_shoes = Item.create!(
-  name: "yellow_shoes",
-  size: "M",
-  user: soda,
-  category: Item::CATEGORIES[2],
-  )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/424035/item/44_424035.jpg')
-yellow_shoes.photo.attach(io: file4, filename: 'yellow_shoes', content_type: 'image/jpg')
-
-
-shoes3 = Item.create!(
-  name: "shoes3",
-  size: "M",
-  user: soda,
-  category: Item::CATEGORIES[2],
-  )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/420127/item/08_420127.jpg')
-shoes3.photo.attach(io: file4, filename: 'shoes3', content_type: 'image/jpg')
-
-
-shoes4 = Item.create!(
-  name: "shoes3",
-  size: "M",
-  user: soda,
-  category: Item::CATEGORIES[2],
-  )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/424021/item/56_424021.jpg')
-shoes4.photo.attach(io: file4, filename: 'shoes4', content_type: 'image/jpg')
-
-
-p 6
-
-bottoms = Item.create!(
-  name: "bottoms",
-  size: "M",
-  user: soda,
-  category: Item::CATEGORIES[1],
-  )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/427423/sub/427423_sub7_popup.jpg')
-bottoms.photo.attach(io: file4, filename: 'bottoms', content_type: 'image/jpg')
-
-
-bottoms2 = Item.create!(
-  name: "jeans",
-  size: "M",
-  user: soda,
-  category: Item::CATEGORIES[1],
-  )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/421376/item/66_421376.jpg')
-bottoms2.photo.attach(io: file4, filename: 'bottoms2', content_type: 'image/jpg')
-
-bottoms3 = Item.create!(
-  name: "jeans_2",
-  size: "M",
-  user: soda,
-  category: Item::CATEGORIES[1],
-  )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/421625/item/69_421625.jpg')
-bottoms3.photo.attach(io: file4, filename: 'bottoms3', content_type: 'image/jpg')
-
-bottoms4 = Item.create!(
-  name: "jeans_2",
-  size: "M",
-  user: soda,
-  category: Item::CATEGORIES[1],
-  )
-file4 = URI.open('https://im.uniqlo.com/images/jp/pc/goods/427000/item/68_427000.jpg')
-bottoms4.photo.attach(io: file4, filename: 'bottoms4', content_type: 'image/jpg')
-
+file = URI.open('https://res.cloudinary.com/drihevibr/image/upload/v1583975704/glasses.png')
+glasses.photo.attach(io: file, filename: 'glasses', content_type: 'image/jpg')
 
 
 puts "finish"
